1ec7b11f80482307ab77bf5461fb65aad7d91549
Cyanoid <cyanoid2024@gmail.com>
2025-06-07 21:52:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 from Parser import parse
 from Parser import tree_to_dict
-from Parser import pre_parse
 from Generator import Generator
 # from z3 import *
 import json
@@ -18,15 +17,18 @@ def parse_file(model_name):
     return parsed_model
 
 
+
 header = """
 // head
 
+#include <cstdlib>
+#include <ctime>
 // #include <enumeration>
 #include <functional>
 #include <iostream>
 #include<iomanip>
 #include <memory>
-#include <mpi.h>
+//#include <mpi.h>
 #include <optional>
 #include <random>
 #include <string>
@@ -39,28 +41,65 @@ header = """
 
 // head-port
 
-enum Direction { in, out };
+int in = 0;
+int out = 1;
 #include <iostream>
 #include <optional>
 
 template <typename T>
 struct Port {
-    Direction p_direction;
+    int p_direction;
     bool reqRead = false;
     bool reqWrite = false;
-    std::optional<T> value;
+    // std::optional<T> value;
+    T value;
 
-    Port(Direction dir, T init_val) 
+    // neighborhood of ports
+    std::vector<std::shared_ptr<Port<T>>> nbhd;
+    void addNbhd(std::shared_ptr<Port<T>> neighbor) {
+        nbhd.push_back(neighbor);
+    }
+    
+
+    Port(int& dir, T& init_val) 
         : p_direction(dir), value(init_val) {}
 
-    Port(Direction dir) 
+    Port(int& dir) 
         : p_direction(dir) {}
 };
 
-void Sync(Port<T> port1, Port<T> port2) {
-    // Synchronize two ports
+template <typename T1>
+void Sync(Port<T1> port1, Port<T1> port2) {
+    port1.addNbhd(port2);
+    port2.addNbhd(port1);
 }
 
+template <typename T3>
+void perform(Port<T3> port) {
+    // Perform some operation with the port
+    if (port.p_direction == in && port.reqRead) {
+        for (const auto& portnb : port.nbhd) {
+            if (portnb->p_direction == out) {
+                
+                port.value = portnb->value; // Assuming we want to read the value
+            }
+        }
+    }
+    if (port.p_direction == out && port.reqWrite) {
+        for (const auto& portnb : port.nbhd) {
+            if (portnb->p_direction == in) {
+                
+                portnb->value = port.value;
+            }
+        }
+        
+    }
+}
+
+template <typename T2>
+void perform(Port<T2> port){
+
+}
 
 // head-transition
 using Statement = std::function<void()>;
@@ -70,19 +109,45 @@ struct Transition {
     Guard guard;
     std::vector<Statement> statements;
 };
-// Transition transition; // not here
 
 using TransitionGroup = std::vector<Transition>;
-// TransitionGroup group; // not here
 
 using TransitionMap = std::vector<TransitionGroup>;
-// TransitionMap map; // not here
+
 // PROBLEM: ONLY GROUP-TRANSITION IS SUPPORTED
-// lan de gai le hao fan
+
+void executeTransition(const Transition& transition) {
+    for (const auto& statement : transition.statements) {
+        statement();
+    }
+}
+
+void processTransitionMap(const TransitionMap& transitionMap) {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    size_t groupIndex = 0;
+
+    while (true) {
+        const TransitionGroup& group = transitionMap[groupIndex];
+        // choose a random transition from the current group
+        size_t transitionIndex = rand() % group.size(); 
+        const Transition& transition = group[transitionIndex];
+
+        // guard returns true, execute the transition
+        if (transition.guard()) {
+            executeTransition(transition);
+            break; 
+        }
+
+        // move to the next group
+        groupIndex = (groupIndex + 1) % transitionMap.size();
+    }
+}
+
 """
 
 def main():
     parsed_model = parse_file('models/leader_3.med')
+    
 
     generator = Generator(parsed_model)
     cpp_code = header
